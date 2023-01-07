75f83e213d0a628c3f07489a4e007215225e4a6f
shmawilton <90579487+shmawilton@users.noreply.github.com>
2023-01-07 05:48:05 +0300

    second commit

--- project/script.js
+++ project/script.js
@@ -6,6 +6,40 @@
 
 // Environment
 
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > nav.offsetHeight + 300) {
+        nav.classList.add('active');
+        // menu.classList.add('active');
+    } else {
+        nav.classList.remove('active');
+        // menu.classList.remove('active');
+    }
+});
+
+// Select all cards
+const cards = document.querySelectorAll('.card');
+
+// Add scroll event listener
+window.addEventListener('scroll', function () {
+    // Get current scroll position
+    let scrollTop = document.documentElement.scrollTop;
+
+    // Loop through cards
+    cards.forEach(card => {
+        // Get card's offset top
+        let cardTop = card.offsetTop;
+
+        // If card is in view
+        if (scrollTop > cardTop - window.innerHeight) {
+            // Animate card
+            card.style.transform = `translateX(0)`;
+        } else {
+            // If not in view, reset card's position
+            card.style.transform = `translateX(${window.innerWidth}px)`;
+        }
+    });
+});
 
 
 // Farming
